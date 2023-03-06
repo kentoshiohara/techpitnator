acd9eadf1cc552651bb6259bacce05b2e6a3c2cb
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-03-06 16:18:28 +0900

    質問・回答画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "games#new"
-  resources :games, only:[:new, :create]
-
+  resources :games, only:[:new, :create] do
+    resource :progresses, only:[:new, :create]
+  end
 end
